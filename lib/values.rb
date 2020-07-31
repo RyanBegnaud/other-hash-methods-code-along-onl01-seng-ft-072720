ef062ae051a542eb7af4ccbd19ea98dcbcb58d0c
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-31 16:55:06 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -10,5 +10,9 @@ groceries = {
 
 def get_the_values(groceries)
   #code your solution here!
+<<<<<<< HEAD
   return groceries.values.flatten
+=======
+  return groceries.values.join(" ")
+>>>>>>> 9c0e6d70aa4c1f162bf8462589604cb4b4f65744
 end
